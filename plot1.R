cf0e47e778f603e60705bf7bab4637c0e352f58e
alok <alokd3@yahoo.com>
2014-10-12 20:56:58 -0400

    first commit

---
+++ plot1.R
@@ -0,0 +1,12 @@
+source("power_data.R")
+plot1 <- function(data=NULL) {
+if(is.null(data))
+data <- power_data()
+png("plot1.png", width=400, height=400)
+hist(data$Global_active_power,
+main="Global Active Power",
+xlab="Global Active Power (kilowatts)",
+ylab="Frequency",
+col="red")
+dev.off()
+}
